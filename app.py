d00bf4954bd07deef32f3455c4d9f2a357335bc4
Abimanyu <raheksa.abimanyu@gmail.com>
2024-04-24 12:39:53 +0700

    Home page

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+
+html_temp= """
+            <div style="">
+                <h1>Employee Promotion Prediction App</h1>
+                <h4>This website belongs to HR Team</h4>
+            </div>
+            """
